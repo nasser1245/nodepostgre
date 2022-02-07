b6bbe7064b8a72eae90ff0cb90f70aced77af933
nasser1245 <nasser1245@gmail.com>
2022-02-07 18:55:38 +0330

    First Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended:"true"}));
+
+app.get("/",(req,res)=>{
+    res.json({message:"Welcome to nasser1245 codeSample."});
+});
+
+const PORT  = process.env.port || 3000;
+
+app.listen(PORT , ()=>{
+    console.log(`Listening on port ${PORT }`);
+});
